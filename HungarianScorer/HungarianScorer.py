4bf387f1cd462a29f6faf430fc4de1c6272de9a6
JohnnyWeymouth <johnnycweymouth@gmail.com>
2024-10-11 15:47:34 -0600

    remove numpy dependancy

--- HungarianScorer/HungarianScorer.py
+++ HungarianScorer/HungarianScorer.py
@@ -1,4 +1,3 @@
-import numpy as np
 from typing import Callable, Any
 from scipy.optimize import linear_sum_assignment
 
@@ -22,21 +21,21 @@ class HungarianScorer():
                 listB += [None] * (len(listA) - len(listB))
 
         # Score each matchup
-        scores = np.zeros((len(listA), len(listB)))
+        scores = [[0 for _ in range(len(listB))] for _ in range(len(listA))]
         for i, itemA in enumerate(listA):
             for j, itemB in enumerate(listB):
                 if (itemA is not None) and (itemB is not None):
-                    scores[i, j] = scoringFunc(itemA, itemB, *args)
+                    scores[i][j] = scoringFunc(itemA, itemB, *args)
                 else:
-                    scores[i, j] = -1e9  # Assign a very low finite score to dummy pairings
+                    scores[i][j] = -1e9  # Assign a very low finite score to dummy pairings
 
         # Use the Hungarian algorithm to find the optimal assignments
-        rowInd, colInd = linear_sum_assignment(-scores)
+        rowInd, colInd = linear_sum_assignment([[-score for score in row] for row in scores])
 
         # Format the best combination into a readable form
         bestCombination = []
         for i, j in zip(rowInd, colInd):
             if (listA[i] is not None) and (listB[j] is not None):
-                matchupScore = scores[i, j]
+                matchupScore = scores[i][j]
                 bestCombination.append((i, j, matchupScore))
         return bestCombination
